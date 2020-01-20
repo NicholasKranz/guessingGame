c33f191b4cd96e3d814c75ab791f1faf6fde6325
Nicholas K <55371819+NicholasKranz@users.noreply.github.com>
2020-01-20 10:12:03 -0500

    Add files via upload

---
+++ Guessing Game.py
@@ -0,0 +1,12 @@
+from random import randint
+randomNum = randint(0,9)
+
+Your_guess = (input("your guess"))
+
+if randomNum == Your_guess :
+    print('Correct!')
+
+else :
+    print('try again')
+
+
